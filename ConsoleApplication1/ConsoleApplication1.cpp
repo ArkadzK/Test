698b7cc995a2c0053d5449406dcae5e1e06d159c
ArkadzK <3100226@gmail.com>
2018-12-26 18:27:20 +0200

    Сбой MSVS...

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,11 +13,13 @@
 //#define PCOM(a)		COM##a
 #define FLASH_SECTOR_MIN		1
 #define FLASH_SECTOR_MAX		23
+#define DEC						(uint8_t)0
+#define HEX						(uint8_t)1
 
 using namespace std;
 
 HANDLE hSerial;
-
+uint8_t scanCOMports(void);
 uint8_t serial_init(void);
 uint8_t read_command(char *arr);
 uint8_t select_act(void);
@@ -25,7 +27,8 @@ uint8_t* prep_uart_msg(uint8_t size);
 uint8_t find_separ(uint8_t i, uint8_t limit);
 uint8_t is_digit(char* arr, uint8_t len);
 uint8_t is_digit_hex(char* arr, uint8_t len);
- void tx_buff_form (uint8_t* buf, uint8_t* data, uint8_t size);
+void tx_buff_form (uint8_t* buf, uint8_t* data, uint8_t size);
+uint8_t number_of_digit(char* arr, uint8_t type);
 
 static char buff[64] = { 0 };
 static struct _TxBuff
@@ -40,27 +43,69 @@ static struct _TxBuff
 	uint8_t* tx_data = NULL;
 }transData;
 
+wchar_t name[] = { L'C',L'O',L'M',L'0',L'\0',L'\0' };
+wchar_t number[] = { L'0',L'1',L'2',L'3',L'4',L'5',L'6' ,L'7' ,L'8' ,L'9' };
+LPCTSTR sPortName;
+
+
 int main()
 {
-	////////////////////////////////
-	int i = 0;
-	/////////////////////////////
+	uint8_t repeat_action_flag = 1;
+
 	uint8_t err = 0;
 	uint8_t len = 0;
-	if ((err=serial_init()) != 0)
-	{
-		cout << "ERROR SerialPortInit " << err << endl;
-		return err;
+	/* Scanning of COM-ports, setup parameters	*/
+	while(repeat_action_flag)
+	{ 
+		if ((err=serial_init()) != 0)
+		{
+			printf("ERROR SerialPortInit! Press any key for repeat or (N) for exit");
+			name[4] = '\0';
+			char c = getchar();
+			if (c == 'N' || c == 'n') return 1;
+		}
+		else 
+		{
+			repeat_action_flag = 0;
+			printf("Init COM%c%c OK\n",name[3],name[4]);
+		}
 	}
-	else printf("Init SerialPort COM3");
-	
-	if ((len = read_command(buff)) >= 1)
-	{		
-		if (prep_uart_msg(len - 1) != NULL)  printf("MSG READY \n") ;
-		else if (transData.comm == 1) printf("MSG READY  CLear ALL\n");
-	}
-	else printf("Uncorrect input\n");
 
+	/* recieve command  */
+	repeat_action_flag = 1;
+	while (repeat_action_flag)
+	{
+		printf("Enter the command:\n");
+		printf("\t : clear all\\sector\\page	NOTHING\\number(dec)\\address(hex)\n");
+		printf("\t : read start_address(hex) end_address(hex)\n");
+		printf("\t : write sector\\page number(dec)\\start_address(hex) NOTHING\\end_address(hex)\n ");
+		if ((len = read_command(buff)) )
+		{
+			if (prep_uart_msg(len - 1) != NULL)
+			{
+				repeat_action_flag = 0;
+				printf("MSG READY \n");// sendMSG()
+			}
+			else if (transData.comm == 1)
+			{
+				repeat_action_flag = 0;
+				printf("MSG READY  CLear ALL\n");// sendMSG()
+			}
+			else
+			{
+				printf("Uncorrect input! Press any key for repeat or (N) for exit");
+
+				char c = getchar();
+				if (c == 'N' || c == 'n') return 1;
+			}
+		}
+		else
+		{
+			printf("Uncorrect input! Press any key for repeat or (N) for exit");
+			char c = getchar();
+			if (c == 'N' || c == 'n') return 1;
+		}
+	}
 		
 //	free(uart_msg);
 //	getchar();
@@ -69,17 +114,107 @@ int main()
 
 uint8_t serial_init(void)
 {
+//	wchar_t *nbr;// = { L'C',L'O',L'M',L'3',L'\0' };
+//	LPCTSTR sPortName = L"COM3";
+	///////////////////////////////////////////////////////////////////////////////
 
-	LPCTSTR sPortName = L"COM3";
-//	for (char i = 31; i < 33; i++)
-//	{
-//	char i = '3';
-//		uint8_t cnt_av_ports = 0;
+	uint8_t er = 1;
+	printf(" the follows COM ports are available: ");
+	for (int ind = 0; ind < 100; ++ind)
+	{
+		if (ind < 10)
+		{
+//			wchar_t name[] = { L'C',L'O',L'M',L'0',L'\0' };
+			name[3] = number[ind];
+		}
+		else
+		{
+//			wchar_t name[] = { L'C',L'O',L'M',L'0',L'0',L'\0' };
+			name[4] = number[ind % 10];
+			name[3] = number[ind / 10];
+		}
+		LPCTSTR sPortName = name;
+		
+
+		hSerial = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+		if (hSerial == INVALID_HANDLE_VALUE)
+		{
+
+			if (GetLastError() == ERROR_FILE_NOT_FOUND)
+			{
+//				printf("serial port %d does not exist.\n", ind);
+				//return err = 1;
+				
+			}
+//			printf("some other error occurred.\n");
+			continue;
+			//return err = 2;
+		}
+		else
+		{
+			//			cnt_av_ports++;
+
+						printf("\tCOM%d", ind);
+			er = 0;
+		}
+		CloseHandle(hSerial);
+	}
+	if (er == 1) printf(" NO \n");
+	else
+	{
+		printf("\n Input the number of COM");
+		char nb[2] = { 0 };
+		uint8_t len = 0;
+		if ((len= read_command(buff))<3) 
+			if (is_digit(buff, len) == 0)
+			{				
+				name[3] = number[(uint8_t)(buff[0] - '0')];
+				name[4] = (buff[1]? number[(uint8_t)(buff[1] - '0')] : '\0');
+				buff[0] = '\0';
+				buff[1] = '\0';
+		/*		LPCTSTR*/ sPortName = name;
+				hSerial = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+				if (hSerial == INVALID_HANDLE_VALUE)
+				{
+					CloseHandle(hSerial);
+					printf("ERROR of opening COM%c%c\n", name[3],name[4]);
+					er = 1;
+
+				}
+				else
+				{
+					DCB dcbSerialParams = { 0 };
+					dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+					if (!GetCommState(hSerial, &dcbSerialParams))
+					{
+						printf("getting state error\n");
+					}
+					dcbSerialParams.BaudRate = CBR_115200;
+					dcbSerialParams.ByteSize = 8;
+					dcbSerialParams.StopBits = ONESTOPBIT;
+					dcbSerialParams.Parity = NOPARITY;
+					if (!SetCommState(hSerial, &dcbSerialParams))
+					{
+						printf("error setting serial port state\n");
+					}
+
+				}
+			}
+		
+
+	}
+		return er;
+	////////////////////////////////////////////////////////////////////////////
+/*
+	uint8_t scanCOMports();
+	LPCTSTR sPortName = nbr;
 		uint8_t err;
-//		LPCTSTR sPortName = L/**/PCOM(3);
+
 		hSerial = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 		if (hSerial == INVALID_HANDLE_VALUE)
 		{
+
 			if (GetLastError() == ERROR_FILE_NOT_FOUND)
 			{
 				printf("serial port does not exist.\n");
@@ -96,7 +231,7 @@ uint8_t serial_init(void)
 			err = 0;
 		}
 		return err;
-//	}
+*/
 }
 
 /*
@@ -114,6 +249,7 @@ uint8_t read_command(char* arr)
 {
 	uint8_t len = 0;
 	char c;
+	
 	do
 	{
 		c = getchar();
@@ -122,7 +258,7 @@ uint8_t read_command(char* arr)
 		*arr = c;
 		arr++;
 		len++;
-		putchar(c);
+//		putchar(c);
 	} while ((c != '\0')&&(c != '\n'));
 
 	return len;
@@ -158,7 +294,13 @@ uint8_t* prep_uart_msg(uint8_t size)
 		{
 		case 'A': transData.comm = 1; transData.lengh_msg = 0; break;
 // clear page address size( in bytes )
-		case 'P': transData.comm = 2; transData.lengh_msg = 8; i = find_separ(i + 1, size - 1);
+		case 'P': transData.comm = 2; transData.lengh_msg = 4; i = find_separ(i + 1, size - 1);
+			if (number_of_digit(&buff[i], HEX) != 4)
+			{
+				printf("Uncorrect Address input\n");
+				return NULL;
+
+			}
 			if (is_digit_hex(buff + i, 4) == 0)
 			{
 				for (uint8_t ind = i; ind < (i + 4); ind++)
@@ -177,24 +319,31 @@ uint8_t* prep_uart_msg(uint8_t size)
 		{
 			transData.comm = 3; transData.lengh_msg = 1; i = find_separ(i + 1, size - 1);
 			if (is_digit(buff + i, 2) == 0)
-				
+			{ 
 				st[0] = buff[i]; st[1] = buff[i + 1];   uint8_t sector = atoi(st); 
 				if ((sector < FLASH_SECTOR_MAX) && (sector > FLASH_SECTOR_MIN))
 				{
-					transData.sector_start =  sector; transData.sector_end = sector;
+//					transData.sector_start =  sector; transData.sector_end = sector;
 	//				uint8_t size = transData.lengh_msg;
 					transData.tx_data = (uint8_t*)malloc(transData.lengh_msg);
-					tx_buff_form(transData.tx_data, &transData.sector_start, transData.lengh_msg);
-					printf("ClearSECTOR %d\n", sector);
+//					tx_buff_form(transData.tx_data, &transData.sector_start, transData.lengh_msg);
+					*(transData.tx_data) = sector;
+					printf("Clear SECTOR %d\n", sector);
 				}
 
-				else printf("Sector number ERROR\n"); break;
+				else
+				{
+					printf("WRONG Sector number!\n");
+					return NULL;
+				}
+			}
+			else
 			{
-				printf("WRONG Sector ADDRESS\n");
+				printf("WRONG Sector format!\n"); return NULL;
 			}
 			break;
 		}
-		default: printf("ERROR_Clear\n"); break;
+		default: printf("ERROR_Clear command\n"); break;
 		} 
 	} break;
 	case 'R': i = find_separ(i, size - 1) + 1; break;
@@ -253,10 +402,30 @@ void tx_buff_form(uint8_t * buf, uint8_t* data, uint8_t size)
 	int i = 0;
 	while (size--)
 	{
-		buf[i] = data[i];
+		buf[i] = data[i] - '0';
 		i++;
 	}
 
+}
+
+uint8_t number_of_digit(char* arr, uint8_t type)
+{
+	uint8_t cnt = 0;
+	if (type )
+	{ 
+		while (((arr[cnt] > 0x2f) && (arr[cnt] < 0x3a)) || ((buff[cnt] < 'G') && (buff[cnt] > '@')))
+		{
+			cnt++;
+		}
+	}
+	else
+	{
+		while (((arr[cnt] > 0x2f) && (arr[cnt] < 0x3a)))
+		{
+			cnt++;
+		}
+	}
+	return cnt;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
